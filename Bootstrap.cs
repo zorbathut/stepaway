4101759aa467e24cc5b2ab34943082de443ace64
Ben Rog-Wilhelm <zorba-git20110501@pavlovian.net>
2016-11-12 00:21:29 -0800

    Initial checkin, no functionality.

---
+++ Bootstrap.cs
@@ -0,0 +1,31 @@
+﻿using CommunityCoreLibrary_ImTheWorkerNow;
+using System;
+using System.Reflection;
+using UnityEngine;
+using Verse;
+
+namespace ImTheWorkerNow
+{
+    class Bootstrap : Def
+    {
+        static Bootstrap()
+        {
+            try
+            {
+                {
+                    /*MethodInfo method1 = typeof(RimWorld.Building_CommsConsole).GetMethod("GetFloatMenuOptions", BindingFlags.Instance | BindingFlags.Public);
+                    MethodInfo method2 = typeof(Building_CommsConsole_Detour).GetMethod("GetFloatMenuOptions", BindingFlags.Static | BindingFlags.Public);
+                    if (!Detours.TryDetourFromTo(method1, method2))
+                    {
+                        Log.Error("EVERYTHING IS BROKEN 1");
+                        return;
+                    }*/
+                }
+            }
+            catch (Exception)
+            {
+                Log.Error("something is seriously wrong");
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+# Step Away From The Medicine
+Rimworld "Step Away From The Medicine" mod
+
+Changes default medicine settings.
